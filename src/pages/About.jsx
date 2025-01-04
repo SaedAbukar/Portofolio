b616554610f8209cf6b897694375475cd414b9c0
Saed Abukar <saed.a.abukar@gmail.com>
2025-01-04 22:44:36 +0200

    Changed the layout int the about section for larger screens

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,36 +23,38 @@ function About() {
   return (
     <div className="section" id="about">
       <h1>About Me</h1>
-      <p>
-        Hello! I’m Saed Abukar, currently pursuing a degree in Software
-        Engineering at Metropolia University of Applied Sciences. With a deep
-        passion for technology and problem-solving, I thrive on creating
-        innovative and meaningful projects that make a real impact.
-        <br />
-        Whether it’s developing user-friendly applications or designing
-        efficient systems, I am always eager to learn and apply new skills to
-        overcome real-world challenges.
-        <br />
-        Outside of my studies and coding projects, I’m an avid football player,
-        fitness enthusiast, and nature lover. I believe in balancing both mental
-        and physical growth, which is why I stay active on the field and in the
-        gym, and find inspiration in the outdoors.
-        <br />
-        I’m excited about the future and the opportunities to collaborate,
-        learn, and contribute to projects that bring positive change. I’m always
-        open to connecting with like-minded professionals and curious minds.
-        Feel free to reach out—there’s a lot more to explore, and I’d love to
-        share my journey with you!
-      </p>
-      <div
-        style={{
-          maxWidth: "1200px",
-          width: "100%",
-          margin: "0 auto",
-        }}
-        className="img-slider-container"
-      >
-        <ImageSlider images={IMAGES} />
+      <div>
+        <p>
+          Hello! I’m Saed Abukar, currently pursuing a degree in Software
+          Engineering at Metropolia University of Applied Sciences. With a deep
+          passion for technology and problem-solving, I thrive on creating
+          innovative and meaningful projects that make a real impact.
+          <br />
+          Whether it’s developing user-friendly applications or designing
+          efficient systems, I am always eager to learn and apply new skills to
+          overcome real-world challenges.
+          <br />
+          Outside of my studies and coding projects, I’m an avid football
+          player, fitness enthusiast, and nature lover. I believe in balancing
+          both mental and physical growth, which is why I stay active on the
+          field and in the gym, and find inspiration in the outdoors.
+          <br />
+          I’m excited about the future and the opportunities to collaborate,
+          learn, and contribute to projects that bring positive change. I’m
+          always open to connecting with like-minded professionals and curious
+          minds. Feel free to reach out—there’s a lot more to explore, and I’d
+          love to share my journey with you!
+        </p>
+        <div
+          style={{
+            maxWidth: "1200px",
+            width: "100%",
+            margin: "0 auto",
+          }}
+          className="img-slider-container"
+        >
+          <ImageSlider images={IMAGES} />
+        </div>
       </div>
     </div>
   );
